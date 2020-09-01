5079297ff5fdfbc232a02926ba5da68754f6cd9e
DuffauJC <jeancharles.duffau@gmail.com>
2020-09-01 10:00:43 +0200

    Updates

---
+++ precache-manifest.d0bd6de725ac68a69029f2d28d827379.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38ffd3841320c2ec9c9ea9754efdcf7",
+    "url": "/todolistreact/index.html"
+  },
+  {
+    "revision": "c3b5b732b75522a8319d",
+    "url": "/todolistreact/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c3b5b732b75522a8319d",
+    "url": "/todolistreact/static/js/2.bc06dcd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolistreact/static/js/2.bc06dcd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a696dde7d0e34d6adde9",
+    "url": "/todolistreact/static/js/main.0b32c285.chunk.js"
+  },
+  {
+    "revision": "d8463f5520392ee25265",
+    "url": "/todolistreact/static/js/runtime-main.61eea731.js"
+  }
+]);
